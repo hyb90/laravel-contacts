078144be4d3b1ba5c80f2dc96f60e638fc4cc93c
hyb90 <hyb.90@yahoo.com>
2020-07-22 14:11:13 +0300

    change asset finction to href

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,14 +2,14 @@
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <head>
         <!-- Scripts -->
-        <script src="{{ secure_asset('js/app.js') }}" defer></script>
+        <script src="https://contacts-hummam.herokuapp.com/js/app.js" defer></script>
         
         <!-- Fonts -->
         <link rel="dns-prefetch" href="//fonts.gstatic.com">
         <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
         
         <!-- Styles -->
-        <link href="{{ secure_asset('css/app.css') }}" rel="stylesheet">
+        <link href="https://contacts-hummam.herokuapp.com/css/app.css" rel="stylesheet">
         <meta name='csrf-token' content="{{csrf_token()}}">
         </head>
         
